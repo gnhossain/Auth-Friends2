4b2abaa5b78b3207ee88ba72687e25d835ae811a
gul hossain <spicymangojungle@gmail.com>
2020-03-25 13:00:42 -0700

    fixed app

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Nav from './components/Nav';
+import Login from './components/Login';
+import Logout from './components/Logout'
+import FriendsList from './components/FriendsList';
+import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="App">
+          <Nav/>
+          <Switch>
+            <PrivateRoute exact path="/protected" component={FriendsList} />
+            <Route path="/login" component={Login} />
+            <Route path="/logout" component={Logout}/>
+            <Route component={Login} />
+            <Route component={Logout}/>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
